702fab9b28eff5376f008786eaf5074f43f01e14
lioritan <slfried@cs.technion.ac.il>
2014-11-21 16:59:07 +0200

    Working on tree analysis

---
+++ problems/tree_stats.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 21 14:25:39 2014
+
+@author: liorf
+"""
+
+    
+def calc_node_stats(tree):
+    '''return num of nodes, node IG stats'''
+    pass
+
+def calc_leaf_stats(tree):
+    '''return num of leaf, leaf size stats, leaf error rate stats'''
+    pass
+
+def calc_rec_tree_stats(tree):
+    '''return num of lvl1 trees, num of lvl2 trees(stats per lvl1 tree and total?)
+    difference in IG between nonrec (for lvl1 tree and for lvl2 trees per lvl1 tree+total)'''
+    pass
